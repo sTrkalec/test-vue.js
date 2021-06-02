a391246eea68cc96fa7f78802d1fec3ff98b2321
Gabriel Miranda <vytorgabriel123v@gmail.com>
2021-06-02 12:22:16 -0300

    Starting new project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div  class="" id="app">
+    <div class="container grid-xs py-2">
+      Hello world
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+}
+</script>
+
